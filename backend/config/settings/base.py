2ebbcda70ae2c68efc7b0acbdb1cb9b7e61e0789
stnielse <steven.nielsen@myriad.com>
2026-07-24 12:05:15 -0700

    app installation

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rsvp',
+    'gallery',
+    'pages',
 ]
 
 MIDDLEWARE = [
