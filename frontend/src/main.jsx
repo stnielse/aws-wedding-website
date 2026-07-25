8af32cc6a49d87c2b5f2c7e81b9d5700d0c8e015
stnielse <steven.nielsen@myriad.com>
2026-07-25 12:32:59 -0700

    main react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import RsvpForm from './RsvpForm'
+import Gallery from './Gallery'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rsvpRoot = document.getElementById('rsvp-root')
+if (rsvpRoot) {
+  const props = JSON.parse(document.getElementById('rsvp-data').textContent)
+  createRoot(rsvpRoot).render(<RsvpForm {...props} />)
+}
+
+const galleryRoot = document.getElementById('gallery-root')
+if (galleryRoot) {
+  const props = JSON.parse(document.getElementById('gallery-data').textContent)
+  createRoot(galleryRoot).render(<Gallery {...props} />)
+}
