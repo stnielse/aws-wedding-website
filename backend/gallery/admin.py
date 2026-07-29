16f7acd8ff8bcc9c9c820bca78ba4182b48cef11
stnielse <steven.nielsen@myriad.com>
2026-07-29 08:34:34 -0600

    photo admin additions

--- backend/gallery/admin.py
+++ backend/gallery/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Photo
 
-admin.site.register(Photo)
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'order', 'uploaded_at')
+    search_fields = ('caption', 'alt_text')
+    ordering = ('order', 'uploaded_at')
