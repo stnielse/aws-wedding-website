9612fd7d3e98256b28501d8878a07c2f7babdcf4
stnielse <steven.nielsen@myriad.com>
2026-07-29 08:34:24 -0600

    rsvp admin additions

--- backend/rsvp/admin.py
+++ backend/rsvp/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-from .models import Guest, RSVP
+from .models import Guest, Party, RSVP
 
-admin.site.register(Guest)
-admin.site.register(RSVP)
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lookup_code')
+    search_fields = ('name', 'lookup_code')
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'party', 'plus_one_allowed', 'email')
+    list_filter = ('plus_one_allowed',)
+    search_fields = ('name', 'party__name', 'email')
+    autocomplete_fields = ('party',)
+
+
+@admin.register(RSVP)
+class RSVPAdmin(admin.ModelAdmin):
+    list_display = ('guest', 'attending', 'meal_choice', 'plus_one_attending', 'submitted_at')
+    list_filter = ('attending', 'meal_choice', 'plus_one_attending')
+    search_fields = ('guest__name', 'guest__party__name')
+    autocomplete_fields = ('guest',)
