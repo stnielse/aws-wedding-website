21822566d1bb5f4ab9b20d51b364cd7975de36b7
stnielse <steven.nielsen@myriad.com>
2026-07-29 08:33:45 -0600

    photo model updates

--- backend/gallery/models.py
+++ backend/gallery/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Photo(models.Model):
     image = models.ImageField(upload_to='gallery/')  # goes to S3 in production
     caption = models.CharField(max_length=300, blank=True)
+    alt_text = models.CharField(max_length=300, blank=True)
     order = models.PositiveIntegerField(default=0)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['order', 'uploaded_at']
+
+    def __str__(self):
+        return self.caption or self.image.name
