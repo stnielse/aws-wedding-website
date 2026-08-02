9b26083da3c88401ea9221a982cd35a0e37012ad
stnielse <steven.nielsen@myriad.com>
2026-08-02 15:47:54 -0600

    infra phase 3 start. tf main

---
+++ infra/phase3/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "1.15.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.54.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = var.project_tag
+      Environment = "phase3"
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
