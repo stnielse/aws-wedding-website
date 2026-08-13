e31c8cc43fd0d456c0c1e59de622277c4f285da8
stnielse <steven.nielsen@myriad.com>
2026-08-13 08:49:55 -0600

    gallery views & urls updates

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rsvp/', include('rsvp.urls')),
+    path('gallery/', include('gallery.urls')),
     path('', include('pages.urls')),
 ]
